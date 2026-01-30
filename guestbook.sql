27c40a5c2ad8f5688aa69ac4d776f65ba4c6d1eb
nedh <nedh.design@gmail.com>
2026-01-30 15:52:04 +0100

    update db

--- guestbook.sql
+++ guestbook.sql
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données : `qhyendxcgr_guestbook`
+-- Base de données : `guestbook`
 --
 
 -- --------------------------------------------------------
